02e9d5ba784f0c35611e6ae782f394ca70fe4214
douglas-camargo <douglascamargo159@gmail.com>
2025-08-06 08:57:37 -0400

    mejora en el responsive

--- src/components/organisms/Skills/Skills.tsx
+++ src/components/organisms/Skills/Skills.tsx
@@ -50,7 +50,7 @@ export const Skills = () => {
 
   return (
     <section id="skills" className="w-full py-16 px-4 md:px-20">
-      <h1 style={{ lineHeight: '150px' }} className={`font-medium xl:indent-[-5px] text-4xl md:text-6xl lg:text-9xl font-['Oswald',Helvetica] mb-3 ${theme === 'dark' ? 'text-white' : 'text-gray-800'}`}>
+      <h1 className={`leading-tight lg:!leading-[150px] font-medium xl:indent-[-5px] text-4xl md:text-6xl lg:text-9xl font-['Oswald',Helvetica] mb-3 ${theme === 'dark' ? 'text-white' : 'text-gray-800'}`}>
         {t('skillsContent.title')}
       </h1>
 
